4e01d497e4f535f2937cab77e2a0da443795e906
babgl <ningarhar@outlook.com>
2016-06-17 23:07:59 +0000

    FrontEnd

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,5 +4,30 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all 
  end
     
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+    
+    def create
+       @recipe = Recipe.new(recipe_params)
+       @recipe.chef = Chef.find(8)
+       
+       if @recipe.save
+      flash[:success] = "Uw Recept is aangemaakt"
+      redirect_to recipes_path
+      elsif render :new
+  end
+      
+    end
+    
+     private 
+     def recipe_params
+    params.require(:recipe).permit(:naam,:overzicht, :ingredient,:beschrijving)
+    end
+    
     
 end
